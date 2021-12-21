0db1b84c37d1da67ef10a6d6c53cd6a7535f3b3a
Nakibaman <nakibaman@gmail.com>
2021-12-21 22:27:46 +0600

    update of Day 4
    update of Day 4

--- Day_4_Task_2/main.c
+++ Day_4_Task_2/main.c
@@ -3,11 +3,10 @@
 
 int main()
 {
-    int number_1,number_2;
-    printf("Enter a number");
-    scanf("%d",&number_1);
-    printf("Enter another number");
-    scanf("%d",&number_2);
-    printf("The added result is %d",number_1+number_2);
+    int number_1,number_2,number_3,result;
+    printf("Enter 3 numbers");
+    scanf("%d%d%d",&number_1,&number_2,&number_3);
+    result=number_1+number_2+number_3;
+    printf("The multiplied result is %d",result);
     return 0;
 }
