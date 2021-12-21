3ea0eb5b19a6d43770c100d3b5fc5ad81dacd261
Nakibaman <nakibaman@gmail.com>
2021-12-21 21:29:48 +0600

    Day 4
    Day 4

--- Day_4_Task_1/main.c
+++ Day_4_Task_1/main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 
 int main()
 {
-    int number_1,number_2;
+    int number_1,number_2,number_3;
     number_1=34;
     number_2=456;
-    printf("The added result of %d and %d is %d",number_1,number_2,number_1+number_2);
+    number_3=500;
+    printf("The added result of %d, %d and %d is %d",number_1,number_2,number_3,number_1+number_2+number_3);
     return 0;
 }
---
+++ Day_4_Task_2/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int number_1,number_2;
+    printf("Enter a number");
+    scanf("%d",&number_1);
+    printf("Enter another number");
+    scanf("%d",&number_2);
+    printf("The added result is %d",number_1+number_2);
+    return 0;
+}
