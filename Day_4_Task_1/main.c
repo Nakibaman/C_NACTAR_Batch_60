aa50c9eb52f5508d55d2649e37d21d2f586fadd7
Nakibaman <nakibaman@gmail.com>
2021-12-21 20:47:19 +0600

    Day 4
    Day 4

---
+++ Day_4_Task_1/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int number_1,number_2;
+    number_1=34;
+    number_2=456;
+    printf("The added result of %d and %d is %d",number_1,number_2,number_1+number_2);
+    return 0;
+}
