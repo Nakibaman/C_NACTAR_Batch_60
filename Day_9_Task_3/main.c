862203007f009edd39f5e2633b26b8a46ead4659
Nakibaman <nakibaman@gmail.com>
2021-12-28 21:50:27 +0600

    Day 9 task 3
    Day 9 task 3

--- Day_9_Task_3/main.c
+++ Day_9_Task_3/main.c
@@ -6,9 +6,9 @@ int main()
     char input;
     printf("Enter a character:");
     scanf("%c",&input);
-    if(input=='a'||input=='A')
-        printf("The character is a/A");
+    if(input=='a'||input=='A'||input='e'||input='E'||input=='i'||input=='I'||input='o'||input=='O'||input=='u'||input=='U')
+        printf("The character is vowel");
     else
-        printf("The character is not a/A");
+        printf("The character is consonent");
     return 0;
 }
